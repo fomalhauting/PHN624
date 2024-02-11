258c2451521b69f4574f918ac8b3ab4ffbad8c65
Kriti <75844962+fomalhauting@users.noreply.github.com>
2024-02-11 23:46:04 +0530

    Update associated-laguerre.py

--- Tutorial1/associated-laguerre.py
+++ Tutorial1/associated-laguerre.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 
 X = np.arange(-15,15,0.1)
 
-def associatedlaguerre(n, k, x):
+def associated(n, k, x):
   ans = 0
   for m in range(n+1):
     ans += (-1**m)*(np.math.factorial(n+k)/(np.math.factorial(n-m)*np.math.factorial(k+m)*np.math.factorial(m)))*(x**m)
@@ -11,7 +11,7 @@ def associatedlaguerre(n, k, x):
 
 for n in range(6):
   for k in range(n+1):
-    L = [associatedlaguerre(n,k,x) for x in X]
+    L = [associated(n,k,x) for x in X]
     plt.plot(X,L)
 
 plt.xlim(-10,10)
