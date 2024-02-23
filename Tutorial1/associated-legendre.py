b23564a5cb85784f347d9c27d150cda92b6e20cd
Kriti <75844962+fomalhauting@users.noreply.github.com>
2024-02-23 15:17:48 +0530

    Update associated-legendre.py

--- Tutorial1/associated-legendre.py
+++ Tutorial1/associated-legendre.py
@@ -9,6 +9,13 @@ def doublefactorial(n):
 
 X = np.arange(-2, 2, 0.01)
 
+def associate_legendre(l,m,x):
+    if l < abs(m): return x - x
+    if m < 0: return (-1) ** abs(m) * f(l - abs(m)) / f(l + abs(m)) * associate_legendre(x, l, abs(m))
+    if l == m: return (-1) ** l * df(2 * l - 1) * (1 - x ** 2) ** (l / 2)
+    if l == m+1: return x * (2 * m + 1) * associate_legendre(x, m, m)
+    return (x * (2 * l - 1) * associate_legendre(x, l-1, m) - (l + m - 1) * associate_legendre(x, l-2, m)) / (l - m)
+'''
 def associated(l, m, x):
     if l == 0:
         return 1
@@ -26,7 +33,7 @@ def associated(l, m, x):
           return x*(2*l+1)*associated(l, l, x)
         else:
           return (x*(2*l-1)*associated(l-1,m,x) - (l+m-1)*associated(l-2, m, x))/(l-m)
-
+'''
 plt.figure(figsize=(8, 6))
 for l in range(6):
     for m in range(l+1):
